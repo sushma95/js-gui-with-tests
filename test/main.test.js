69f79821b2395ad6fb7bc76ada6caa40709084ed
mprogers <mprogers@mac.com>
2019-09-25 11:20:57 -0500

    added some comments

--- test/main.test.js
+++ test/main.test.js
@@ -8,22 +8,29 @@ QUnit.test('TEST add', assert => {
 
 QUnit.config.autostart = false  // sync = false; start after loading html
 
+// This script, calls when the page loads, reaches out to the app that we wish to test
+// It basically pastes the contents of that page into *this* web page, whew! This shows
+// how we manipulate the DOM.
+
+// The openingTag and closingTag specify which part of the original app's web page that we grab here
+// Pretty slick eh?
+
 window.addEventListener('load', () => {
-  const appURL = '../index.html'
+  const appURL = '../index.html' // reach out to the html for the app (js-gui)
   const openingTag = '<main class="container mt-5 flex-fill">'
-  const closingTag = '</main>'
+  const closingTag = '</main>' // go grab it!
   fetch(appURL, { method: 'GET' })
     .then(response => {
       return response.text() // returns promise
     })
-    .then(txt => {
+    .then(txt => {                
       const start = txt.indexOf(openingTag)
       const end = txt.indexOf(closingTag) + closingTag.length
-      const html = txt.substring(start, end)
-      const qunitFixtureBody = document.getElementById('qunit-fixture')
-      qunitFixtureBody.innerHTML = html
-      console.info(qunitFixtureBody)
-      QUnit.start()
+      const html = txt.substring(start, end) // we only want part of the page
+      const qunitFixtureBody = document.querySelector('#qunit-fixture')
+      qunitFixtureBody.innerHTML = html // put the page into the DOM - the second div associated with this page
+      console.info(qunitFixtureBody) // print it out so we can see it (it doesn't get inserted into the page)
+      QUnit.start() // start the actual testing - it finds and runs both the tests, defined in QUnit.test()
     })
     .catch(error => {
       console.error('error:', error);
